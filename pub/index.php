f7c77f6ce2a017898bb39bd732c37f984f9f41ec
Ilja Polanskis <ilja.polanskis@pearlconvert.no>
2023-02-15 23:46:13 +0200

    Init Commit

---
+++ pub/index.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+const ROOT_DIR = __DIR__ . '/../';
+const APP_DIR = ROOT_DIR . 'app/';
+
+echo "Hello World";
