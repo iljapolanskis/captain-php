bbebea8c02e9feca6abd37129137548dc5a91613
Ilja Polanskis <ilja.polanskis@pearlconvert.no>
2023-05-29 15:20:40 +0300

    Added build directly

---
+++ public/build/app.c31af1f9.js
@@ -0,0 +1,8 @@
+"use strict";
+(self.webpackChunkcaptain_php = self.webpackChunkcaptain_php || []).push([[143], {
+    642: () => {
+    }
+}, p => {
+    var a;
+    a = 642, p(p.s = a)
+}]);
---
+++ public/build/ck-editor.589e387a.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcaptain_php=self.webpackChunkcaptain_php||[]).push([[725],{966:(e,c,r)=>{var t=r(234);r.n(t)().create(document.querySelector("#editor")).catch((function(e){console.error(e)}))}},e=>{e.O(0,[234],(()=>{return c=966,e(e.s=c);var c}));e.O()}]);
